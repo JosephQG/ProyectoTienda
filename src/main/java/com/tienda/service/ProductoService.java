4867de36923e19e1f1602e36850728af28dbb870
JosephQG <jquiros60376@ufide.ac.cr>
2025-10-11 10:45:09 -0600

    Semana 5 CRUD Completo de Producto

---
+++ src/main/java/com/tienda/service/ProductoService.java
@@ -0,0 +1,67 @@
+package com.tienda.service;
+
+import com.tienda.domain.Producto;
+import com.tienda.repository.ProductoRepository;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class ProductoService {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @Transactional(readOnly = true)
+    public List<Producto> getProductos(boolean activo) {
+        if (activo) {
+            return productoRepository.findByActivoTrue();
+        }
+        return productoRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Producto> getProducto(Integer idProducto) {
+        return productoRepository.findById(idProducto);
+    }
+
+    @Transactional
+    public void delete(Integer idProducto) {
+        // se valida que el idProducto exista...
+        if (!productoRepository.existsById(idProducto)) {
+            //se lanza una excepcion que no esta el idProducto
+            throw new IllegalArgumentException("La producto" + idProducto + " no existe...");
+
+        }
+        try {
+            productoRepository.deleteById(idProducto);
+        } catch (DataIntegrityViolationException e) {
+            throw new IllegalStateException("No se puede eliminar la producto, tiene datos asociados" + e);
+        }
+    }
+
+    @Autowired
+    private FirebaseStorageService firebaseStorageService;
+
+    //este metodo tiene doble funcion.. si idProducto esta vacio, se inserta el registro
+    //si idProducto, tiene informacion, se actualiza el registro
+    @Transactional
+    public void save(Producto producto, MultipartFile imagenFile) {
+        producto = productoRepository.save(producto);
+        if (!imagenFile.isEmpty()) {
+            try {
+                String rutaImagen = firebaseStorageService.uploadImage(imagenFile, "producto", producto.getIdProducto());
+                producto.setRutaImagen(rutaImagen);
+                productoRepository.save(producto);
+            } catch (IOException e) {
+
+            }
+        }
+    }
+    
+}
